ace71e639bbbf7c18a6d3f3e78657d8b1087ede2
Avbarkova <avbarkova@gmail.com>
2023-04-16 23:02:39 +0300

    What season

--- src/what-season.js
+++ src/what-season.js
@@ -12,29 +12,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 function getSeason(date) {
-  if (date === null) {
-    return ('Unable to determine the time of year!');
-  } else {
-      if (Object.prototype.toString.call(date) !== '[object Date]') {
-        return ('Invalid date!');
-      } else {
-        let month1 = date.getMonth();
-        let result = season(month1);
-        return result;
-        }
-    }
+  if (date === undefined)/*(!(date instanceof Date))*/ return ('Unable to determine the time of year!');
+  try {
+    date.toDateString();
+  } catch {
+    throw Error('Invalid date!');
+  }
+  /*Object.prototype.toString.call(date) !== '[object Date]' || date.getMilliseconds()===0 || date.getMonth()===NaN || Date.prototype.toDateString.call(date) !== date.toDateString())|| date.toString()!==Date.prototype.toDateString.call(date) {throw new Error ('Invalid date!');}*/
   
-  
+    let month1 = date.getMonth();
+    let result = season(month1);
+    return result;
   /*для определения сезона по месяцу*/
   function season (month) {
-
     let seasonOfYear = '';
-    console.log (month);
     if (0<=month<=1 || month==11) {seasonOfYear = 'winter';} 
     if (month>=2 && month<=4) {seasonOfYear = 'spring';} 
     if (month>=5 && month<=7) {seasonOfYear = 'summer';} 
     if (month>=8 && month<=10) {seasonOfYear = 'autumn'};
-    
     return (seasonOfYear);
   }
     
